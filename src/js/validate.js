c54740d82633e82c0e39252e28a754180255b0c2
Alexander <vinokurov.ag@mail.ru>
2019-10-17 18:25:52 +0300

    Делаю валидацию

--- src/js/validate.js
+++ src/js/validate.js
@@ -1,1 +1,25 @@
+var applicationForm = document.querySelector('.form-application');
+var applicationFormSubmitBtn = applicationForm.querySelector('.form-application__btn');
 
+var validateApplicationForm = function () {
+  var inputs = applicationForm.querySelectorAll('.form__input');
+
+  inputs.forEach(function (input) {
+    if (!input.value) {
+      input.setCustomValidity('Необходимо заполнить данное поле');
+      input.style.border = '1px solid red';
+    } else {
+      input.setCustomValidity('');
+      input.style.border = 'none';
+    }
+  });
+};
+
+applicationFormSubmitBtn.addEventListener('click', function () {
+  validateApplicationForm();
+});
+
+applicationForm.addEventListener('submit', function (evt) {
+  evt.preventDefault();
+  alert('olololo!');
+})
