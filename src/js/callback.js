97660ab7bbe10d509835e58155a1ea2113d18d69
Alexander <vinokurov.ag@mail.ru>
2019-10-21 23:39:53 +0300

    Сделал блок popup, работаю над окном успешной отправки формы

--- src/js/callback.js
+++ src/js/callback.js
@@ -3,12 +3,12 @@ var openBtn = document.querySelector('.features-list__link--callback');
 var closeBtn = document.querySelector('.callback-modal__close-btn');
 
 var openWidnowBtnHandler = function () {
-  callbackModalWindow.classList.toggle('callback-modal--show');
+  callbackModalWindow.classList.toggle('popup--show');
 };
 
 var closeWindowBtnHandler = function () {
-  if (callbackModalWindow.classList.contains('callback-modal--show')) {
-    callbackModalWindow.classList.remove('callback-modal--show');
+  if (callbackModalWindow.classList.contains('popup--show')) {
+    callbackModalWindow.classList.remove('popup--show');
   }
 };
 
