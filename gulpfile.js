b8bc001dc00968cbacd771c78c8ddfecb977fdcb
Alexander <vinokurov.ag@mail.ru>
2019-10-25 15:13:03 +0300

    Добавил в сборку минификацию css и js

--- gulpfile.js
+++ gulpfile.js
@@ -11,6 +11,16 @@ var svgstore = require("gulp-svgstore");
 var rename = require("gulp-rename");
 var posthtml = require("gulp-posthtml");
 var include = require("posthtml-include");
+var csso = require("gulp-csso");
+var jsminify = require("gulp-minify");
+
+gulp.task("jsminify", function () {
+  return gulp.src("src/js/*.js")
+  .pipe(jsminify({
+    noSource: true
+  }))
+  .pipe(gulp.dest("build/js"))
+});
 
 gulp.task("html", function(){
   return gulp.src("src/*.html")
@@ -19,6 +29,7 @@ gulp.task("html", function(){
   ]))
   .pipe(gulp.dest("build"));
 });
+
 gulp.task("sprite", function() {
   return gulp.src([
     "src/img/*.svg",
@@ -29,6 +40,7 @@ gulp.task("sprite", function() {
   .pipe(rename("sprite.svg"))
   .pipe(gulp.dest("build/img"));
 });
+
 gulp.task("css", function() {
     return gulp.src("src/sass/style.scss")
     .pipe(sourcemap.init())
@@ -36,13 +48,33 @@ gulp.task("css", function() {
     .pipe(postcss([
         autoprefixer()
     ]))
+    .pipe(csso())
+    .pipe(rename("style.min.css"))
     .pipe(sourcemap.write("."))
     .pipe(gulp.dest("build/css"))
-    .pipe(server.stream());
+    .pipe(server.stream())
 });
+
+gulp.task("normalize", function () {
+  return gulp.src("src/css/normalize.css")
+  .pipe(csso())
+  .pipe(rename("normalize.min.css"))
+  .pipe(gulp.dest("build/css"));
+});
+
+gulp.task("fancybox", function () {
+  return gulp.src("src/css/jquery.fancybox.css")
+  .pipe(csso())
+  .pipe(rename("jquery.fancybox.min.css"))
+  .pipe(gulp.dest("build/css"));
+});
+
+
+
 gulp.task("del-build", function() {
     return del("build");
 });
+
 gulp.task("server", function() {
     server.init ({
         server: "build/",
@@ -57,23 +89,23 @@ gulp.task("server", function() {
     gulp.watch("src/css/**/*.{css}", gulp.series("copy", "refresh"));
     gulp.watch("src/*.html", gulp.series("html", "refresh"));
 });
+
 gulp.task ("refresh", function (done) {
     server.reload();
     done();
 });
+
 gulp.task("copy", function() {
     return gulp.src([
         "src/fonts/**/*.{woff,woff2}",
         "src/img/**",
-        "src/js/**",
         "src/*.html",
-        "src/css/normalize.css",
-        "src/css/jquery.fancybox.css"
     ], {
         base: "src"
     })
     .pipe(gulp.dest("build"));
 });
 
-gulp.task("build", gulp.series("del-build", "copy", "css", "sprite", "html"));
+gulp.task("minify", gulp.series("css", "normalize", "fancybox", "jsminify"));
+gulp.task("build", gulp.series("del-build", "copy", "minify", "sprite", "html"));
 gulp.task("start", gulp.series("build", "server"));
